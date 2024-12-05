6c1cc503fa13f9c71c440fff93712cacdc7a35a7
Ahmad Naufal Waskito Aji <ahmadnaufal11705@gmail.com>
2024-12-05 16:12:32 +0700

    pls bisa

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 $request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$base_path = '/news_app'; // Sesuaikan dengan base path aplikasi Anda
+$base_path = '/Basdat-News'; // Sesuaikan dengan base path aplikasi Anda
 $path = str_replace($base_path, '', $request_uri); // Hilangkan base path dari URL
 $segments = explode('/', $path);
 $id = isset($segments[2]) ? $segments[2] : null;
